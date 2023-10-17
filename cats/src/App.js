7c447a837c589be0c213eeb72191bf507ec41131
pauleinrique <56940023+pauleinrique@users.noreply.github.com>
2023-10-17 18:02:44 -0300

    Ajuste no display flex
    Adicionei o flex-wrap para acertar o layout

--- cats/src/App.js
+++ cats/src/App.js
@@ -5,14 +5,12 @@ import './index.css';
 export default function App(){
   return(
     <div className="app-container">
-      <div>
+      
         <Card />
         <Card />
-      </div>
-      <div>
         <Card />
         <Card />
-      </div>
+      
       
     </div>
   )
