5e075cd78255ff509e34053b735163361853317f
pauleinrique <56940023+pauleinrique@users.noreply.github.com>
2023-10-24 12:42:54 -0300

    Ajuste no projeto cats
    Ajustei os dados do projeto cats para que os dados também estivem sendo disponibilizados atráves do .map para treinar a utilização da função

--- cats/src/App.js
+++ cats/src/App.js
@@ -1,16 +1,18 @@
 import React from "react"
 import Card from "./Card"
 import './index.css';
+import dataCats from "./dataCats"
 
 export default function App(){
+  const Gatos = dataCats.map(item => {
+    return (
+      <Card img={item.img} name={item.name} phone={item.phone} mail={item.mail}/>
+    )
+  })
   return(
     <div className="app-container">
       
-        <Card img="gato1.jpg" name="Mr. Whiskerson" phone="(212) 555-1234" mail="mr.whiskaz@catnap.meow"/>
-        <Card img="gato2.jpeg" name="Fluffykins" phone="(212) 555-2345" mail="fluff@me.com"/>
-        <Card img="gato3.jpg" name="Felix" phone="(212) 777-4567" mail="thecat@hotmail.com"/>
-        <Card img="gato4.jpeg" name="Pumpkin" phone="(0800) CAT KING" mail="pumpkin@scrimba.com"/>
-      
+      {Gatos}
       
     </div>
   )
---
+++ cats/src/dataCats.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 1,
+        img: "gato1.jpg",
+        name: "Mr. Whiskerson",
+        phone: "(212) 555-1234",
+        mail: "mr.whiskaz@catnap.meow"
+
+    },
+    {
+        id: 2,
+        img: "gato2.jpeg",
+        name: "Fluffykins",
+        phone: "(212) 555-2345",
+        mail: "fluff@me.com"
+
+    },
+    {
+        id: 3,
+        img: "gato3.jpg",
+        name: "Felix",
+        phone: "(212) 777-4567",
+        mail: "thecat@hotmail.com"
+
+    },
+    {
+        id: 4,
+        img: "gato4.jpeg",
+        name: "Pumpkin",
+        phone: "(0800) CAT KING",
+        mail: "pumpkin@scrimba.com"
+
+    }
+
+]
