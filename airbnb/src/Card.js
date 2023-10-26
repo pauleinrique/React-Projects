030c6724b198e9de1e8fa81dfe47bfc84adddae9
pauleinrique <56940023+pauleinrique@users.noreply.github.com>
2023-10-25 23:25:10 -0300

    Projeto airbnb finalizado
    Todos ajustes finalizados no projeto

--- airbnb/src/Card.js
+++ airbnb/src/Card.js
@@ -1,17 +1,19 @@
 import React from "react"
 
-export default function Card({img, rating, reviewCount, country, title, price}){
+export default function Card(props){
+    
     return(
         <div className="card--container">
-            <img src={img} alt="card" className="card--photo"></img>
+            {props.item.openSpots === 0 ?<div className="card--badge">SOLD OUT</div>: <div className="card--badge">AVAILABLE</div> }
+            <img src={props.item.coverImg} alt="card" className="card--photo"></img>
             <div className="card--stats">
                 <img src="Star 1-1.png" alt="star" className="card--star"></img>
-                <span>{rating}</span>
-                <span className="gray">{reviewCount} • </span>
-                <span className="gray"> {country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">{props.item.stats.reviewCount} • </span>
+                <span className="gray"> {props.item.location}</span>
             </div>
-            <p className="card--title">{title}</p>
-            <p> <span className="bold card--title">{price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p> <span className="bold card--title">{props.item.price}</span> / person</p>
         </div>
     )
 }
--- airbnb/src/Header.js
+++ airbnb/src/Header.js
@@ -6,14 +6,11 @@ import Data from "./data"
 export default function Header(){
     const dataElements = Data.map(item => {
         return (
-            <Card img={item.coverImg} 
-            rating={item.stats.rating} 
-            reviewCount={item.stats.reviewCount} 
-            country={item.location} 
-            title={item.title} 
-            price={item.price} >
+            <Card 
+            key = {item.id}
+            item={item}
 
-            </Card>
+            />
         )
     })
 
