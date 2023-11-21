83ba9c1390a3cf8fb9aa0fdd792c7159a2681646
pauleinrique <56940023+pauleinrique@users.noreply.github.com>
2023-11-21 17:55:45 -0300

    Iniciando o projeto meme generator

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./Header.js"
+import Meme from "./Meme.js"
 
 export default function App (){
   return(
     <div>
       <Header />
+      <Meme />
     </div>
   )
 }
---
+++ meme-generator/src/Meme.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Meme(){
+    return(
+        <div className="meme--form">
+            <form>
+                <div className="meme--textboxes--container">
+                <input type="text" alt="part 1 meme" placeholder="shut up..." className="meme--textboxes"></input>
+                <input type="text" alt="part 1 meme" placeholder="and..." className="meme--textboxes"></input>
+                </div>
+                <div className="meme--button-container">
+                <button type="submit" className="meme--button">Get a new meme image</button>
+                </div>
+            </form>
+        </div>
+    )
+}
