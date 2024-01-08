865db467560d496147e002b1d247841870314b18
pauleinrique <56940023+pauleinrique@users.noreply.github.com>
2024-01-08 17:46:54 -0300

    atualizações no gerador de meme

--- meme-generator/src/Meme.js
+++ meme-generator/src/Meme.js
@@ -1,17 +1,25 @@
 import React from "react"
+import memesData from "./memesData.js"
 
 export default function Meme(){
+    let url
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random()* memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
     return(
         <div className="meme--form">
-            <form>
+            <div className="form">
                 <div className="meme--textboxes--container">
                 <input type="text" alt="part 1 meme" placeholder="shut up..." className="meme--textboxes"></input>
                 <input type="text" alt="part 1 meme" placeholder="and..." className="meme--textboxes"></input>
                 </div>
                 <div className="meme--button-container">
-                <button type="submit" className="meme--button">Get a new meme image 👀</button>
+                <button type="submit" className="meme--button" onClick={getMemeImage}>Get a new meme image 👀</button>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
